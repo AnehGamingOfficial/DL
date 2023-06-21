74f3e7d7d68960bf0f97920e495217a7e059b6d5
AnehGamingOfficial <116832438+AnehGamingOfficial@users.noreply.github.com>
2023-06-21 22:55:41 +0800

    Update js.js

--- pages/js.js
+++ pages/js.js
@@ -5,16 +5,3 @@ script.setAttribute('data-cfasync', 'false');
 script.src = '//ophoacit.com/1?z=6064929';
 document.head.appendChild(script);
 //END
-function vngbanner() {
-  var d = 'goomaphy.com';
-  var z = 6065114;
-  var s = document.createElement('script');
-  s.src = 'https://' + d + '/401/' + z;
-  
-  try {
-    (document.body || document.documentElement).appendChild(s);
-  } catch (e) {
-    console.error('Failed to append script:', e);
-  }
-}
-///end
