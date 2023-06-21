f6466b6ab1f5ddbdb6a2c2558d0100bc15707b58
AnehGamingOfficial <116832438+AnehGamingOfficial@users.noreply.github.com>
2023-06-21 18:01:33 +0800

    Update js.js

--- pages/js.js
+++ pages/js.js
@@ -1,36 +1,20 @@
-$('.buttonContainer').click(function(){
-	if($('.buttonContainer').hasClass('active')){
-		$(this).removeClass('active');
-		$('.complete').removeClass('fadein');
-		$('#counter').fadeOut(100);
-		$('.ball').fadeOut(100);
-		count().stop;
-	} else{
-		$(this).addClass('active');
-		$('#counter').fadeIn(200);
-		$('.ball').fadeIn(200);
-		count();
-	}
-});
-
-//Loading
-function count(){
-	$({countNum: $('#counter').text()}).animate({countNum: 100}, {
-	  duration: 5000,
-	  easing:'linear',
-	  step: function() {
-		 $('#counter').text(Math.floor(this.countNum) + '%');
-	  },
-	  complete: function() {
-		 $('#counter').fadeOut(200);
-		 $('.complete').addClass('fadein');
-		 $('.ball').fadeOut(200);
-		  $('#button').fadeOut(100);
-		  setTimeout(function() {
-			   $('.buttonContainer').removeClass('active');
-			  $('.complete').removeClass('fadein');
-			  $('#button').fadeIn(200);
-   		}, 1000);
-	  }
-	});
-}
+$(function() {
+  var btn = $(".btn");
+  
+  btn.on("click", function() {
+    
+    $(this).addClass('btn-progress');
+    setTimeout(function() {
+      btn.addClass('btn-fill')
+    }, 500);
+    
+    setTimeout(function() {
+      btn.removeClass('btn-fill')
+    }, 4100);
+    
+    setTimeout(function() {
+      btn.addClass('btn-complete')
+    }, 4100);
+  
+  });
+})
