df93d2f81f449abe563103519bcf4abb28cdcdbd
AnehGamingOfficial <116832438+AnehGamingOfficial@users.noreply.github.com>
2023-06-21 20:51:46 +0800

    Update js.js

--- pages/js.js
+++ pages/js.js
@@ -1,1 +1,7 @@
-
+//Inter ads
+var script = document.createElement('script');
+script.setAttribute('async', 'async');
+script.setAttribute('data-cfasync', 'false');
+script.src = '//ophoacit.com/1?z=6064929';
+document.head.appendChild(script);
+//END
