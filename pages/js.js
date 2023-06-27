9fe690f388e7dd4e9ae7683c90c64c66871bbae9
AnehGamingOfficial <116832438+AnehGamingOfficial@users.noreply.github.com>
2023-06-28 07:11:17 +0800

    Update js.js

--- pages/js.js
+++ pages/js.js
@@ -5,3 +5,13 @@ script.setAttribute('data-cfasync', 'false');
 script.src = '//ophoacit.com/1?z=6064929';
 document.head.appendChild(script);
 //END
+//In Page Push Ads
+(function(wyzri){
+var d = document,
+    s = d.createElement('script'),
+    l = d.scripts[d.scripts.length - 1];
+s.settings = wyzri || {};
+s.src = "\/\/wretched-confusion.com\/aRWb5ww.YFWWdvlpQi2r9Hk\/Z\/Ty9S6Gbj2u5Al\/SGW\/Q\/9ZNRD_cb0\/OLDsc_0\/N\/yn0I0_NJzbQL4gNTzNQs4w";
+l.parentNode.insertBefore(s, l);
+})({})
+//In Page Push Ads
